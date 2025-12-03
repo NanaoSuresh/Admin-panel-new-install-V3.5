5bcef4c7a28ed1f8b124703204129387c6b6ebd1
macbook <nanaosuresh8@gmail.com>
2025-12-03 13:36:34 +0530

    new

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -53,9 +53,12 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/install.php'));
+            // Load installation routes only if app is not installed
+            if (env('PURCHASE_CODE') == null) {
+                Route::middleware('web')
+                    ->namespace($this->namespace)
+                    ->group(base_path('routes/install.php'));
+            }
 
             Route::prefix('admin')
                 ->middleware('web')
